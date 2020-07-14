c2b12d569851c891780c893d6a4b545aa403f299
Josué David Hernández Ramírez <justinhernandez054@gmail.com>
2020-07-13 23:26:29 -0500

    CRUD DB

--- qrscanner/lib/src/providers/db_provider.dart
+++ qrscanner/lib/src/providers/db_provider.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:qrscanner/src/models/scan_model.dart';
 
 class DBProvider {
   //Creacion de instancia para que este disponible de manera global
@@ -35,5 +36,70 @@ class DBProvider {
           'valor TEXT'
           ')');
     });
+  }
+
+  //CREAR REGISTROS
+  nuevoScanRaw(ScanModel nuevoScan) async {
+    final db = await database;
+
+    final res = await db.rawInsert("INSERT Into Scans(id, tipo, valor) "
+        "VALUES (${nuevoScan.id}, '${nuevoScan.tipo}', '${nuevoScan.valor})' ;");
+    return res;
+  }
+
+  //Crear insserciones mediante JSON
+  nuevoScan(ScanModel nuevoScan) async {
+    final db = await database;
+    final res = db.insert('Scan', nuevoScan.toJson());
+    return res;
+  }
+
+  //SELECT - Obtener información
+  Future<ScanModel> getScanId(int id) async {
+    final db = await database;
+    final res = await db.query('Scans', where: 'id = ?', whereArgs: [id]);
+    return res.isNotEmpty ? ScanModel.fromJson(res.first) : null;
+  }
+
+  Future<List<ScanModel>> getTodosScans() async {
+    final db = await database;
+    final res = await db.query(
+      'Scans',
+    );
+
+    List<ScanModel> list =
+        res.isNotEmpty ? res.map((e) => ScanModel.fromJson(e)).toList() : [];
+    return list;
+  }
+
+  Future<List<ScanModel>> getScansporTipo(String tipo) async {
+    final db = await database;
+    final res = await db.rawQuery("SELECT * FROM Scans WHERE tipo='$tipo'");
+
+    List<ScanModel> list =
+        res.isNotEmpty ? res.map((e) => ScanModel.fromJson(e)).toList() : [];
+    return list;
+  }
+
+  //ACTUALIZAR REGISTROS
+  Future<int> updateScan(ScanModel nuevoScan) async {
+    final db = await database;
+    //Solo se actualiza el Scan que se manda con ID
+    final res = await db.update('Scans', nuevoScan.toJson(),
+        where: 'id=?', whereArgs: [nuevoScan.id]);
+    return res;
+  }
+
+  //BORRAR REGISTROS
+  Future<int> deleteScan(int id) async {
+    final db = await database;
+    final res = await db.delete('Scans', where: 'id=?', whereArgs: [id]);
+    return res;
+  }
+
+  deleteAll() async {
+    final db = await database;
+    final res = await db.rawDelete('DELETE FROM Scans');
+    return res;
   }
 }
